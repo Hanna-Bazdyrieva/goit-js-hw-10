ed8b7b41f3990cdfe2ef92f989a22dace7e5d357
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-03-05 19:59:34 +0100

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,13 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import getRefs from './getRefs';
 
-const BASE_URL = 'https://restcountries.com/v3.1';
+// const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountries(country) {
   getRefs().list.innerHTML = '';
   getRefs().info.innerHTML = '';
   return fetch(
-    `${BASE_URL}/name/${country}?fields=name,capital,population,flags,languages`
+    `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`
   )
     .then(response => {
       if (!response.ok) {
